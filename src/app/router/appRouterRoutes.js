c707d2a7506bb52c615d8656be4a0cc35d8cb131
Oleksandr Shaposhnykov <94079420+Salmix1326@users.noreply.github.com>
2025-09-21 21:11:28 +0200

    Update appRouterRoutes.js

--- src/app/router/appRouterRoutes.js
+++ src/app/router/appRouterRoutes.js
@@ -1,12 +1,12 @@
 import { frontRoutes } from "@/shared/config/routes/frontRoutes";
 
 const pagesList = Object.keys(frontRoutes.pages);
-const pages = import.meta.glob("@/pages/*.jsx");
+const pages = import.meta.glob("/src/pages/*.jsx");
 
 export const appRouterRoutes = pagesList.map((page) => ({
   ...frontRoutes.pages[page],
   lazy: async () => {
-    const key = `@/pages/${page}.jsx`;
+    const key = `/src/pages/${page}.jsx`;
     if (!pages[key]) {
       throw new Error(`Page not found: ${key}`);
     }
