44ae84b9a2e9f560d7d799454fc875b84add5539
Oleksandr Shaposhnykov <94079420+Salmix1326@users.noreply.github.com>
2025-09-21 20:48:29 +0200

    Update appRouterRoutes.js

--- src/app/router/appRouterRoutes.js
+++ src/app/router/appRouterRoutes.js
@@ -1,10 +1,39 @@
 import { frontRoutes } from "@/shared/config/routes/frontRoutes";
+import HomePage from "@/pages/HomePage.jsx";
+import LoginPage from "@/pages/LoginPage.jsx";
+import UsersPage from "@/pages/UsersPage.jsx";
+import PostsPage from "@/pages/PostsPage.jsx";
+import NotFoundPage from "@/pages/NotFoundPage.jsx";
+import ForbiddenPage from "@/pages/ForbiddenPage.jsx";
+import GlobalErrorPage from "@/pages/GlobalErrorPage.jsx";
 
-const pagesList = Object.keys(frontRoutes.pages);
-
-export const appRouterRoutes = pagesList.map((page) => ({
-  ...frontRoutes.pages[page],
-  lazy: async () => ({
-    Component: (await import(`@/pages/${page}.jsx`)).default,
-  }),
-}));
+export const appRouterRoutes = [
+  {
+    ...frontRoutes.pages.HomePage,
+    Component: HomePage,
+  },
+  {
+    ...frontRoutes.pages.LoginPage,
+    Component: LoginPage,
+  },
+  {
+    ...frontRoutes.pages.UsersPage,
+    Component: UsersPage,
+  },
+  {
+    ...frontRoutes.pages.PostsPage,
+    Component: PostsPage,
+  },
+  {
+    ...frontRoutes.pages.NotFoundPage,
+    Component: NotFoundPage,
+  },
+  {
+    ...frontRoutes.pages.ForbiddenPage,
+    Component: ForbiddenPage,
+  },
+  {
+    ...frontRoutes.pages.GlobalErrorPage,
+    Component: GlobalErrorPage,
+  },
+];
