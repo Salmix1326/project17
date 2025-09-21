4aae8422e47a8ca367506a8bfaf41135e7deea18
Oleksandr Shaposhnykov <94079420+Salmix1326@users.noreply.github.com>
2025-09-21 20:43:48 +0200

    Update appRouterRoutes.js

--- src/app/router/appRouterRoutes.js
+++ src/app/router/appRouterRoutes.js
@@ -1,12 +1,10 @@
 import { frontRoutes } from "@/shared/config/routes/frontRoutes";
 
 const pagesList = Object.keys(frontRoutes.pages);
-const pages = import.meta.glob("../../pages/*.jsx");
 
 export const appRouterRoutes = pagesList.map((page) => ({
   ...frontRoutes.pages[page],
-  lazy: async () => {
-    const mod = await pages[`../../pages/${page}.jsx`]();
-    return { Component: mod.default };
-  },
+  lazy: async () => ({
+    Component: (await import(`@/pages/${page}.jsx`)).default,
+  }),
 }));
